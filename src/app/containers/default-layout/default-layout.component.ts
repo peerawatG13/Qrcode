4b76a3b31a25aff5723c97d5927f5accb03980f5
Peerawat13 <peerawat.kr@rmuti.ac.th>
2021-10-12 21:43:08 +0700

    line liff test

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,6 +1,7 @@
 import {Component , OnInit} from '@angular/core';
 import { navItems } from '../../_nav';
-import liff from '@line/liff';
+import liff from '@line/liff'
+
 
 @Component({
   selector: 'app-dashboard',
@@ -11,13 +12,40 @@ export class DefaultLayoutComponent implements OnInit {
   public navItems = navItems;
 
   // line
-
-  toggleMinimize(e) {
-    this.sidebarMinimized = e;
-  }
-  
+  title = 'angular-line-login';
+  idToken = '';
+  displayName = '';
+  pictureUrl = '';
+  statusMessage = '';
+  userId = '';
   ngOnInit(){
-    
+    this.initLine();
   }
-  
+  initLine(): void {
+    liff.init({ liffId: '' }, () => {
+      if (liff.isLoggedIn()) {
+        this.runApp();
+      } else {
+        liff.login();
+      }
+    }, err => console.error(err));
+  }
+
+  runApp(): void {
+    const idToken = liff.getIDToken();
+    this.idToken = idToken;
+    liff.getProfile().then(profile => {
+      console.log(profile);
+      this.displayName = profile.displayName;
+      this.pictureUrl = profile.pictureUrl;
+      this.statusMessage = profile.statusMessage;
+      this.userId = profile.userId;
+    }).catch(err => console.error(err));
+  }
+
+  logout(): void {
+    liff.logout();
+    window.location.reload();
+  }
+
 }
