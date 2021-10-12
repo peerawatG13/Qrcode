e9451c18b4558938dfeb5915874abaf8b65dc93a
Peerawat13 <peerawat.kr@rmuti.ac.th>
2021-10-13 02:07:22 +0700

    new test

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,6 +1,6 @@
 import {Component , OnInit} from '@angular/core';
 import { navItems } from '../../_nav';
-import liff from '@line/liff'
+
 
 
 @Component({
@@ -12,40 +12,8 @@ export class DefaultLayoutComponent implements OnInit {
   public navItems = navItems;
 
   // line
-  title = 'angular-line-login';
-  idToken = '';
-  displayName = '';
-  pictureUrl = '';
-  statusMessage = '';
-  userId = '';
   ngOnInit(){
-    this.initLine();
+    
   }
-  initLine(): void {
-    liff.init({ liffId: '' }, () => {
-      if (liff.isLoggedIn()) {
-        this.runApp();
-      } else {
-        liff.login();
-      }
-    }, err => console.error(err));
-  }
-
-  runApp(): void {
-    const idToken = liff.getIDToken();
-    this.idToken = idToken;
-    liff.getProfile().then(profile => {
-      console.log(profile);
-      this.displayName = profile.displayName;
-      this.pictureUrl = profile.pictureUrl;
-      this.statusMessage = profile.statusMessage;
-      this.userId = profile.userId;
-    }).catch(err => console.error(err));
-  }
-
-  logout(): void {
-    liff.logout();
-    window.location.reload();
-  }
-
+  
 }
