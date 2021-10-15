1a47485de8d01478cd8d87a232f60a5c888de48a
Peerawat13 <peerawat.kr@rmuti.ac.th>
2021-10-15 11:15:56 +0700

    liff line

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,5 +1,8 @@
 
+import { HttpClient } from '@angular/common/http';
 import { Component , OnInit } from '@angular/core';
+import liff from '@line/liff/dist/lib';
+import { profile } from 'console';
 import { navItems } from '../../_nav';
 
 
@@ -17,12 +20,38 @@ export class DefaultLayoutComponent implements OnInit {
   toggleMinimize(e) {
     this.sidebarMinimized = e;
   }
-  constructor(){}
+  title : any =''
+  pictureUrl: any = ''
+  displayName: any = ''
+  userId: any = ''
+  idToken: any = ''
+  
+
+  constructor( private http :HttpClient){}
 
   ngOnInit(): void {
+   this.initLine();
+   console.log(this.runApp);
    
   }
-  
-  
+  initLine(): void {
+    liff.init({ liffId: '1656299107-xQZbkJjd' }, () => {
+      if (liff.isLoggedIn()) {
+        this.runApp();
+      } else {
+        liff.login();
+      }
+    }, err => console.error(err));
+  }
+  runApp(): void {
+    const idToken = liff.getIDToken();
+    this.idToken = idToken;
+    liff.getProfile().then(profile => {
+      console.log(profile);
+      this.displayName = profile.displayName;
+      this.pictureUrl = profile.pictureUrl;
+      this.userId = profile.userId;
+    }).catch(err => console.error(err));
+  }
   
 }
